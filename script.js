4e9b0b5991b905a87d3640fece704501f88b33f6
lukamil98 <81568724+lukamil98@users.noreply.github.com>
2022-07-27 14:58:02 +0200

    Add files via upload
    My fitness related webpage.

---
+++ script.js
@@ -0,0 +1,13 @@
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width
+    = '250px';
+    document.getElementById('main').style.marginLeft
+    = '250px';
+}
+
+function closeSlideMenu(){
+    document.getElementById('side-menu').style.width
+    = '0';
+    document.getElementById('main').style.marginLeft
+    = '0';
+}
